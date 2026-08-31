8f7f1134109ddbd523527334b8197ded4840d0e4
luthfyaatika-stack <luthfyaatika@smpn5po.sch.id>
2026-08-31 10:24:11 +0700

    menampilkan kategori di localhost

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
 const PORT = 5000;
 
 app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jelajahpo_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek kedatabase', err);
+    } else {
+        console.log('Berhasil konek kedatabase jelajahpo');
+    }
+});
+
+/////////////////////////ROUTE GET WISATA/////////////////
+app.get('/wisata', (req, res) => {
+    const sql = 'SELECT * FROM wisata';
+    db.query(sql, (err,results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+///////////////////ROUTE GET KATEGORI///////////////////
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err,results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
 
 app.get('/', (req, res) => {
     res.send(' Selamat datang di jelajahpo API   ');
