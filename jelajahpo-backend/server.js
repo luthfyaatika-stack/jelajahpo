95292a9d2c8d97abaefac994adf151fe25b95d15
luthfyaatika-stack <luthfyaatika@smpn5po.sch.id>
2026-08-31 13:30:10 +0700

    penambahan post wisata

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql2');
+const { error } = require('cros/common/logger');
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
@@ -26,6 +29,23 @@ app.get('/wisata', (req, res) => {
     db.query(sql, (err,results) => {
         if (err) return res.status(500).json({ error: err });
         res.json(results);
+    });
+});
+////////////////////////ROUTE POST WISATA////////////////////////
+app.post('wisata', (req, res) => {
+    const { nama_wisata, deskripsi, harga_tiket, id_kategori } = req.body;
+
+    if(!nama_wisata || !harga_tiket) {
+        return res.status(400).json({ message: 'Nama Wisata dan harga_tiket wajib diisi' });
+    }
+
+    const sql = 'INSERT INTO wisata (nama_wisata, deskripsi, harga_tiket, id_kategori, tgl_input) VALUES (?, ?, ?, ?, NOW())';
+    db.query(sql, [nama_wisata, deskripsi, harga_tiket, id_kategori], (err, results) => {
+        if (err) return res.status(500).json({ error: err.sqlMessage });
+        res.json({
+            message: ' List wisata berhasil ditambahkan!',
+            id_wisata: result.insertId
+        });
     });
 });
 
